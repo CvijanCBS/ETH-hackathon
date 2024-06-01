1c0006beddc33f6dc64e4feabb42f5f7207adbe8
RastkoCBS <rastko@ceresblockchain.solutions>
2024-06-01 15:20:06 +0200

    Add verify to hardhat config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+      }
+    ],
+  },
+  networks: {
+      localhost: {
+          chainId: 31337,
+      },
+      sepolia: {
+          url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
+          accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+          //   accounts: {
+          //     mnemonic: MNEMONIC,
+          //   },
+          chainId: 11155111,
+      },
+  },
+  etherscan: {
+      // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+      apiKey: {
+          // npx hardhat verify --list-networks
+          sepolia: ETHERSCAN_API_KEY,
+      },
+  },
 };
